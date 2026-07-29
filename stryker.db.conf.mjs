f339cb2952246b4ceebe3fa18151814a99b6f97c
hoangtu34 <tranbaoviet1007@gmail.com>
2026-07-29 12:21:38 +0700

    test: sequence mutation checks by module type

--- stryker.db.conf.mjs
+++ stryker.db.conf.mjs
@@ -5,8 +5,8 @@ const config = {
 	plugins: ['@stryker-mutator/vitest-runner'],
 	testRunner: 'vitest',
 	mutate: [
-		'src/lib/utils/rules_matcher.ts',
-		'src/lib/utils/dedup.ts'
+		'src/lib/db/repos/transactions.ts',
+		'src/lib/backup/index.ts'
 	]
 };
 
